946863524b61ccf8b22ff52cef641018f347d5e4
JAVICHO\USUARIO <xavi531070@gmail.com>
2018-02-08 05:51:38 -0500

    Ntics2

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+/*
+	Landed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+	
+});
